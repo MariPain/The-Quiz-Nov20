ad8b916a8be28d50c19b206b7c4265db0b2216b2
Mariola Pedraza <mariola.pedraza@gmail.com>
2021-01-07 16:19:19 +0100

    validando js no terminado

--- scripts.js
+++ scripts.js
@@ -1,35 +1,68 @@
-// const respuestaCorrecta = {
-//     reina: "angola",
-//     relaciones: "rusia",
-//     maruja: "g27",
-//     gorillaMask: "guerrilla",
-//     solanas: "warhol"
-// }
-
-
-document.querySelector('form[name= "reina"]').addEventListener('submit', (event) => {
-    event.preventDefault();
-})
-
-document.getElementById("reina").value;
-document.getElementById("relaciones").value;
-document.getElementById("maruja").value;
-document.getElementById("gorillaMask").value;
-document.getElementById("solanas").value;
-
-//cosntruir funcion validar
-
-function validarQuiz() {
-
-    let total = 5;
-    let puntos = 0;
-    let myForm = document.forms["quizForm"];
-    let respuestas = []
-
-
-    return false;
-
-
+// capturo respuestas correctas en variable, usando clave valor
+const respuestaCorrecta = {
+    reina: "angola",
+    relaciones: "rusia",
+    maruja: "g27",
+    gorillaMask: "guerrilla",
+    solanas: "warhol"
 }
 
-//la validacion se hace a raves de submit, pero antes de enviar le decimos que recorra las preguntas y nos diga cual es la respuesta correcta
+// const respuestas = document.getElementsByTagName("input").name.value;
+
+//accedo a mi formulario 
+document.querySelector('form[name="quizForm"]').addEventListener('submit', (event) => {
+            event.preventDefault();
+            for (let i = 0; i < document.getElementsByClassName("reina").length; i++) {
+                if (event.target.elements.reina.value === respuestaCorrecta.value) {
+                    document.getElementById(respuestaCorrecta.reina).style.backgroundColor = "green"
+                    alert("has acertado")
+                } else {
+                    document.getElementById(respuestaCorrecta.reina).style.backgroundColor = "red"
+                    alert("te has equivocado")
+                }
+            }
+
+            for (let i = 0; i < document.getElementsByClassName("relaciones").length; i++) {
+                if (event.target.elements.relacionaes.value === respuestaCorrecta.value) {
+                    document.getElementById(respuestaCorrecta.relaciones).style.backgroundColor = "green"
+                    alert("has acertado")
+                } else {
+                    document.getElementById(respuestaCorrecta.relaciones).style.backgroundColor = "red"
+                    alert("te has equivocado")
+                }
+            }
+            for (let i = 0; i < document.getElementsByClassName("maruja").length; i++) {
+                if (event.target.elements.maruja.value === respuestaCorrecta.value) {
+                    document.getElementById(respuestaCorrecta.maruja).style.backgroundColor = "green"
+                    alert("has acertado")
+                } else {
+                    document.getElementById(respuestaCorrecta.maruja).style.backgroundColor = "red"
+                    alert("te has equivocado")
+                }
+            }
+            for (let i = 0; i < document.getElementsByClassName("gorilaMask").length; i++) {
+                if (event.target.elements.gorillaMask.value === respuestaCorrecta.value) {
+                    document.getElementById(respuestaCorrecta.gorillaMask).style.backgroundColor = "green"
+                    alert("has acertado")
+                } else {
+                    document.getElementById(respuestaCorrecta.gorillaMask).style.backgroundColor = "red"
+                    alert("te has equivocado")
+                }
+            }
+            for (let i = 0; i < document.getElementsByClassName("solanas").length; i++) {
+                if (event.target.elements.solanas.value === respuestaCorrecta.value) {
+                    document.getElementById(respuestaCorrecta.solanas).style.backgroundColor = "green"
+                    alert("has acertado")
+                } else {
+                    document.getElementById(respuestaCorrecta.solanas).style.backgroundColor = "red"
+                    alert("te has equivocado")
+                }
+            }
+        }
+        //accedo a la respuesta
+
+        // document.getElementsByClassName("relaciones").value;
+        // document.getElementsByClassName("maruja").value;
+        // document.getElementsByClassName("reina").value;
+        // document.getElementsByClassName("gorillaMask").value;
+        // document.getElementsByClassName("solanas").value;
